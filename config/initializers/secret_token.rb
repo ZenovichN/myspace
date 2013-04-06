03f51dcf3d18892ace12325551c48f2a6279967c
ZenovichN <zenovich.n@gmail.com>
2013-04-06 13:35:05 +0300

    myspace

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myspace::Application.config.secret_token = '24c36418e51db9025cb82cbe4efc2be1292627fc2b90d25ead5996b71c8ea09da9f4d2a5e92c2aa358089a758a7e7dd35671e52f680fc71166e06027b3e09d6b'
